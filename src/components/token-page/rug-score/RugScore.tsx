1d38f93a7e1def8e939bd55cdf9fa3a831702659
Sione Milhem <89093969+marciano147@users.noreply.github.com>
2024-08-14 02:10:15 +0300

    finished rug score ui

--- src/components/token-page/rug-score/RugScore.tsx
+++ src/components/token-page/rug-score/RugScore.tsx
@@ -1,12 +1,15 @@
 import { FC } from 'react';
-import { Card, Typography, useTheme } from '@mui/material';
+import { Card, Typography, useTheme, IconButton, Tooltip, Box } from '@mui/material';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import RefreshIcon from '@mui/icons-material/Refresh';
 import ScoreLine, { ScoreLineConfig } from './score-line/ScoreLine';
 
 interface RugScoreProps {
     score: number | null;
+    onRecalculate: () => void;
 }
 
-const RugScore: FC<RugScoreProps> = ({ score }) => {
+const RugScore: FC<RugScoreProps> = ({ score, onRecalculate }) => {
     const theme = useTheme();
     const scoreLineRanges: ScoreLineConfig = {
         [theme.palette.error.main]: { start: 0, end: 30 },
@@ -15,10 +18,22 @@ const RugScore: FC<RugScoreProps> = ({ score }) => {
     };
 
     return (
-        <Card sx={{ height: '19vh' }}>
-            <Typography variant="body2" align="center" color="text.secondary">
-                RUG SCORE
-            </Typography>
+        <Card sx={{ height: '19vh', padding: '0px 10px' }}>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Typography variant="body2" color="text.secondary" sx={{ fontWeight: 'bold', mr: 1 }}>
+                        RUG SCORE
+                    </Typography>
+                    <Tooltip title="The Rug Score is an algorithm based on token collection metrics and social media footprints. The score ranges from 1 to 100, with 100 being the best. It represents the collection's transparency and trustworthiness. If you find the score unsatisfactory, you can send a request to review it with the 'Send Request' button near the token header.">
+                        <InfoOutlinedIcon fontSize="small" />
+                    </Tooltip>
+                </Box>
+                <Tooltip title="Click to recalculate the Rug Score.">
+                    <IconButton onClick={onRecalculate} aria-label="recalculate score">
+                        <RefreshIcon />
+                    </IconButton>
+                </Tooltip>
+            </Box>
             {score !== null ? <ScoreLine value={score} config={scoreLineRanges} /> : null}
         </Card>
     );
