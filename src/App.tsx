6a2ba1b7387114d732a46610ba881e98a18d2b88
Dor Zion <dor17.10@gmail.com>
2024-09-11 01:23:37 +0300

    try push directly into develop

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import { ThemeProvider } from '@mui/material/styles';
 import { useCallback, useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { showGlobalSnackbar } from './components/alert-context/AlertContext';
+import Footer from './components/footer/Footer';
 import Navbar from './components/navbar/Navbar';
 import { fetchWalletBalance } from './DAL/KaspaApiDal';
 import { ThemeContext } from './main';
+import BatchTransferPage from './pages/batch-transfer-page/BatchTransferPage';
+import PrivacyPolicy from './pages/compliance/PrivacyPolicy';
+import TermsOfService from './pages/compliance/TermsOfService';
+import TrustSafety from './pages/compliance/TrustSafety';
 import DeployPage from './pages/deploy-page/DeployPage';
 import GridPage from './pages/krc-20/GridPage';
 import PortfolioPage from './pages/portfolio-page/PortfolioPage';
 import TokenPage from './pages/token-page/TokenPage';
 import { darkTheme } from './theme/DarkTheme';
 import { lightTheme } from './theme/LightTheme';
+import { UserVerfication } from './types/Types';
 import { disconnect, isKasWareInstalled, requestAccounts, signMessage, switchNetwork } from './utils/KaswareUtils';
 import {
     generateNonce,
@@ -20,12 +26,6 @@ import {
     setWalletBalanceUtil,
     ThemeModes,
 } from './utils/Utils';
-import Footer from './components/footer/Footer';
-import PrivacyPolicy from './pages/compliance/PrivacyPolicy';
-import TermsOfService from './pages/compliance/TermsOfService';
-import TrustSafety from './pages/compliance/TrustSafety';
-import { UserVerfication } from './types/Types';
-import BatchTransferPage from './pages/batch-transfer-page/BatchTransferPage';
 
 const App = () => {
     const [themeMode, setThemeMode] = useState(getLocalThemeMode());
