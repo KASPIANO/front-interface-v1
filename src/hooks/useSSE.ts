ab1f360a79a290923a403cbbcf1ed8cd5b69b300
Dor Zion <dor17.10@gmail.com>
2024-09-22 00:08:45 +0300

    add sse hook

--- src/hooks/useSSE.ts
+++ src/hooks/useSSE.ts
@@ -2,31 +2,37 @@ import { useEffect, useState } from 'react';
 
 const useSSE = (walletId: string) => {
     const [events, setEvents] = useState([]);
+    const [error, setError] = useState(null);
     const url = import.meta.env.VITE_API_ENDPOINT;
+
     useEffect(() => {
-        // Open SSE connection with customer ID
-        const eventSource = new EventSource(`${url}?walletId=${walletId}`);
+        if (!walletId) return; // Don't open connection if no walletId
+
+        // Open SSE connection with updated customer ID
+        const eventSource = new EventSource(`${url}/events/stream?walletId=${walletId}`);
 
         // Handle incoming messages
         eventSource.onmessage = (event) => {
+            debugger;
             const data = JSON.parse(event.data);
             setEvents((prevEvents) => [...prevEvents, data]);
         };
 
-        // Handle errors (optional, SSE will attempt to reconnect automatically)
+        // Handle errors
         eventSource.onerror = (err) => {
+            setError('EventSource failed to connect.');
             console.error('EventSource failed:', err);
-            eventSource.close(); // Optionally close connection on error
+            eventSource.close(); // Close connection on error
         };
 
-        // Cleanup: Close the connection when the component unmounts
+        // Cleanup: Close the connection if the walletId changes or component unmounts
         return () => {
             console.log(`Closing connection for customer ${walletId}`);
             eventSource.close();
         };
-    }, [url, walletId]);
+    }, [url, walletId]); // Re-run the effect when walletId or URL changes
 
-    return events;
+    return { events, error };
 };
 
 export default useSSE;
