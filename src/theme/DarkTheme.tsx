35acd48f93aa91125ab82333edc45f88f7c4d80b
Sione Milhem <89093969+marciano147@users.noreply.github.com>
2024-10-31 21:32:50 +0200

    done

--- src/theme/DarkTheme.tsx
+++ src/theme/DarkTheme.tsx
@@ -166,7 +166,7 @@ export const darkTheme = createTheme({
                             borderColor: '#49EACB', // Hover border color
                         },
                         '&.Mui-focused fieldset': {
-                            borderColor: '#3c6ed4', // Focused border color
+                            borderColor: '#49EACB', // Focused border color
                         },
                     },
                 },
@@ -179,7 +179,7 @@ export const darkTheme = createTheme({
                 },
                 option: {
                     '&[aria-selected="true"]': {
-                        backgroundColor: '#3c6ed4', // Selected item background
+                        backgroundColor: '#49EACB', // Selected item background
                     },
                     '&:hover': {
                         backgroundColor: '#49EACB', // Hover background color
